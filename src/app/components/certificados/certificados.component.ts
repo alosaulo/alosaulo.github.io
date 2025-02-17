fb27d1a0c92617c05afaed73de85f560c1964da1
Saulo Caliman Gomes <saulocalimangomes@gmail.com>
2025-02-17 10:18:28 -0300

    Modificação em certificados

--- src/app/components/certificados/certificados.component.ts
+++ src/app/components/certificados/certificados.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+interface Certificado {
+  titulo : string;
+  descricao : string;
+  instituicao : string;
+  carga_horaria : string;
+  data_conclusao : string;
+}
 
 @Component({
   selector: 'app-certificados',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './certificados.component.html',
   styleUrl: './certificados.component.scss'
 })
-export class CertificadosComponent {
+export class CertificadosComponent implements OnInit{
+  certificados: Certificado[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Certificado[]>('/data/certificados.json').subscribe(data => {
+      this.certificados = data;
+    }, error => {
+      console.error('Error loading certificados:', error);
+    });
+  }
+  
 }
