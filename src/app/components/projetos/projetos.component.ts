d7fc66abc0bf6642c5ea66af2d15868e81b33f1b
Saulo Caliman Gomes <saulocalimangomes@gmail.com>
2026-02-27 11:47:00 -0300

    Imagens e projeto

--- src/app/components/projetos/projetos.component.ts
+++ src/app/components/projetos/projetos.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+interface Projeto {
+  empresa: string;
+  cargo: string;
+  periodo: string;
+  descricao: string;
+  imagens?: string[];
+}
 
 @Component({
   selector: 'app-projetos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projetos.component.html',
   styleUrl: './projetos.component.scss'
 })
-export class ProjetosComponent {
+export class ProjetosComponent implements OnInit {
+  projetos: Projeto[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Projeto[]>('/data/projetos.json').subscribe({
+      next: (data) => this.projetos = data,
+      error: (err) => console.error('Erro ao carregar projetos:', err)
+    });
+  }
 }
